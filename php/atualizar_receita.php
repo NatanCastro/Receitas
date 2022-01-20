e2fae5fae11b9a8999bb366d6aea111369d674ae
Natan Gabriel Almeida de castro <65430728+natan22gt@users.noreply.github.com>
2022-01-20 16:53:01 -0300

    incluindo WHERE no comando sql de atualização

--- php/atualizar_receita.php
+++ php/atualizar_receita.php
@@ -6,12 +6,12 @@ if (isset($_POST['editar'])) {
   $nome = $_POST['nome'];
   $desc_receita = $_POST['desc_receita'];
 
-  $sql = "UPDATE receitas SET nome=?, desc_receita=?";
+  $sql = "UPDATE receitas SET nome=?, desc_receita=? WHERE id=?";
   $stmt = $conn->prepare($sql);
 
   $stmt->bindParam(1, $nome, PDO::PARAM_STR);
   $stmt->bindParam(2, $desc_receita, PDO::PARAM_STR);
-
+  $stmt->bindParam(3, $id, PDO::PARAM_INT);
   $stmt->execute();
 
   header("location: ../lista_receitas.php");
